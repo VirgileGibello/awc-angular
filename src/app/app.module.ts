a0ff1f57f00eafbdb3f924b047a17a34bc944502
Arnaud RITTI <arnaud.ritti@adeliom.com>
2024-01-22 15:34:57 +0100

    feat: install AWC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { setBasePath } from '@agence-adeliom/awc';
+setBasePath('/');
+
 // @ts-ignore
 @NgModule({
   declarations: [
